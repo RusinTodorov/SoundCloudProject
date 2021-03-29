b27b522e9c781071b4e84682488a8a3cf5504e09
Rusin Todorov <jim_profit@abv.bg>
2021-03-29 16:18:16 +0300

    Adding Sign In and Register pages.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Home/Header/Header';
 import Footer from './components/Initial Page/Footer/Footer';
-import SingIn from './components/SingIn';
-import CreateAccount from './components/CreateAccount';
+import SignIn from './components/SignIn/SignIn';
+import CreateAccount from './components/CreateAccount/CreateAccount';
 import Home from './components/Home/Home';
 import InitialPage from './components/Initial Page/InitialPage';
 import Users from './components/Users';
@@ -21,7 +21,7 @@ function App() {
         <Route path="/Users" component={Users} />
         <Route path="/Upload" component={Upload} />
         <Route path="/Home" component={Home} />
-        <Route path="/SingIn" component={SingIn} />
+        <Route path="/SignIn" component={SignIn} />
         <Route path="/CreateAccount" component={CreateAccount} />
         <Route path="/" exact component={InitialPage} />
         <Route component={PageNotFound} />
